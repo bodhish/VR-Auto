5d3e7f797cb86ecfc1c90e46a28d4dd164c2f4e8
Bodhish Thomas <bodhish@gmail.com>
2018-04-18 17:39:02 +0530

    Update Scripting

---
+++ Assets/RoadCreator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadCreator : MonoBehaviour {
+
+	public GameObject road;
+
+	void Start() {
+    for (int i = 0; i < 5; i++) {
+        Instantiate(road);
+    }
+}
+}
