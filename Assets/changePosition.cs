ce540c9a51b516e05418193e61df6316258a90b2
Bodhish Thomas <bodhish@gmail.com>
2018-04-18 13:28:03 +0530

    Update event settings

---
+++ Assets/changePosition.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changePosition : MonoBehaviour {
+
+	// Define methord for changing position
+	public void GoToNewPosition () {
+		Debug.Log("My Methord was called");
+	}
+}
