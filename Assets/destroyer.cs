223b9a00718e33b7cbb1d46a5805e450722a9888
Bodhish Thomas <bodhish@gmail.com>
2018-04-19 19:22:40 +0530

    Update destroyer script

--- Assets/destroyer.cs
+++ Assets/destroyer.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class destroyer : MonoBehaviour {
 
 	// Use this for initialization
-	
+void Update() {
+        transform.Translate(Vector3.forward*-1 *5* Time.deltaTime);
+        transform.Translate(Vector3.forward *5* Time.deltaTime);
+    }
  void OnCollisionEnter (Collision col)
     {
-			Debug.Log("Game over");
+        if(col.gameObject.tag == "Destroyable")
+        {
             Destroy(col.gameObject);
-    }
+        }
+}
 }
