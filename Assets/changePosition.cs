7bd3228df04e67edc01fc3056059021cc4c1681a
Bodhish Thomas <bodhish@gmail.com>
2018-04-20 15:36:55 +0530

    update change postion script

--- Assets/changePosition.cs
+++ Assets/changePosition.cs
@@ -5,7 +5,17 @@ using UnityEngine;
 public class changePosition : MonoBehaviour {
 
 	// Define methord for changing position
-	public void GoToNewPosition () {
+	public void GoToPositionOne () {
 		Debug.Log("My Methord was called");
 	}
+	public void GoToPositionTwo () {
+		Debug.Log("My Methord was called");
+	}
+	public void GoToPositionThree () {
+		Debug.Log("My Methord was called");
+	}
+	public void GoToPositionFour () {
+		Debug.Log("My Methord was called");
+	}
+
 }
